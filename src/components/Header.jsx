0c6cff392ad55c7894175a2a9126cc2e494fe2e4
Rana Nassar <rana.nassar99@yahoo.com>
2023-03-25 01:41:22 +0200

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,19 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import logo from "../shared/Logo-svg.png";
 
 function Header() {
   return (
     <>
       <Navbar bg="secondary" variant="dark">
         <Container>
+        <img src={logo} alt="logo" className="me-4"  />
           <Navbar.Brand href="/">Home</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/About">About</Nav.Link>
             <Nav.Link href="/">Menu</Nav.Link>
+
           </Nav>
         </Container>
       </Navbar>
