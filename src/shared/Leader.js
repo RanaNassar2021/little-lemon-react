492176ffb928fc204cd0fed8e71ad1b1ece3d97f
Rana Nassar <rana.nassar99@yahoo.com>
2023-03-25 01:51:04 +0200

    image fix

--- src/shared/Leader.js
+++ src/shared/Leader.js
@@ -3,27 +3,27 @@ export const LEADERS =
         {
         id: 0,
         name:'Rana Nassar',
-        image: 'react-menu/assets/images/me.jpg',
+        image: 'https://RanaNassar2021.github.io/little-lemon-react/assets/images/me.jpg',
         description:"Our CEO, Rana, credits her hardworking East Asian immigrant parents who undertook the arduous journey to the shores of America with the intention of giving their children the best future. Her mother's wizardy in the kitchen whipping up the tastiest dishes with whatever is available inexpensively at the supermarket, was her first inspiration to create the fusion cuisines for which The Frying Pan became well known. she brings her zeal for fusion cuisines to this restaurant, pioneering cross-cultural culinary connections.",
          },
         {
         id: 1,
         name:'Mark ',
-        image: 'react-menu/assets/images/mark.jpg',
+        image: 'https://RanaNassar2021.github.io/little-lemon-react/assets/images/mark.jpg',
         description:"ur CFO, Mark, as he is affectionately referred to by his colleagues, comes from a long established family tradition in farming and produce. His experiences growing up on a farm in the Australian outback gave him great appreciation for varieties of food sources. As he puts it in his own words, Everything that runs, wins, and everything that stays, pays!",
 
         },
         {
         id: 2,
         name:'Erica',
-        image: 'react-menu/assets/images/erica.jpg',
+        image: 'https://RanaNassar2021.github.io/little-lemon-react/assets/images/erica.jpg',
         description:"Blessed with the most discerning gustatory sense, Erica, our CFO, personally ensures that every dish that we serve meets his exacting tastes. Our chefs dread the tongue lashing that ensues if their dish does not meet her exacting standards. she lives by her motto, You click only if you survive my lick.",
        
         },
         {
         id: 3,
         name:'Omar',
-        image: 'react-menu/assets/images/omar.jpg',
+        image: 'https://RanaNassar2021.github.io/little-lemon-react/assets/images/omar.jpg',
         description:"Award winning three-star Michelin chef with wide International experience having worked closely with whos-who in the culinary world, he specializes in creating mouthwatering Indo-Italian fusion experiences. He says, Put together the cuisines from the two craziest cultures, and you get a winning hit! Amma Mia!",
         }
     ];
