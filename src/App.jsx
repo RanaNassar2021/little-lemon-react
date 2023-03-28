a1c66817070233fd269f820586781cac7cea9a75
Rana Nassar <rana.nassar99@yahoo.com>
2023-03-28 21:13:39 +0200

    navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
 import React, {Component} from "react";
-import Home from './components/Home';
-import About from './components/AboutComponent';
-import Header from "./components/Header";
+//import Header from "./components/Header";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from "./shared/Logo-svg.png";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/AboutComponent";
+
 class App extends Component{
   
     render(){
-    let component 
-    switch (window.location.pathname) {
-        case "/":
-            component = <Home/>
-            break;
-            case "/About":
-                component = <About/>
-             break;
-             case "/Contact":
-                 component = <Home/>
-             break;
-    
-        default:
-            component = <Home/>
-            break;
-    }
+ 
         return(
-            <div>  
-            <Header/>
-            
-                <div className="row">
-                {component} 
-                </div>
-                </div>                                    
-          
+            <>
+  <Navbar bg="secondary" variant="dark">
+        <Container>
+        <img src={logo} alt="logo" className="me-4"  />
+          <Navbar.Brand href="/">Home</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/About">About</Nav.Link>
+            <Nav.Link href="/">Menu</Nav.Link>
+
+          </Nav>
+        </Container>
+      </Navbar>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/About" element={<About/>}/>
+                    <Route path="/Menu" element={<Home/>}/>   
+                </Routes>
+              
+      
+            </>
         )
     }
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,14 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from "../shared/Logo-svg.png";
+import About from './AboutComponent';
+import { Routes, Route } from "react-router-dom";
+import Home from './Home';
 
 function Header() {
   return (
     <>
+   
       <Navbar bg="secondary" variant="dark">
         <Container>
         <img src={logo} alt="logo" className="me-4"  />
@@ -17,6 +21,13 @@ function Header() {
           </Nav>
         </Container>
       </Navbar>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/About" element={<About/>}/>
+                    <Route path="/Menu" element={<Home/>}/>   
+                </Routes>
+              
+              
 
     </>
   );
